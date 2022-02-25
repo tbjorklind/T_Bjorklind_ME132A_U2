298892368ff0bf519b522f265feb8da3cc570d07
tbjorklind <97900554+tbjorklind@users.noreply.github.com>
2022-02-25 13:46:50 +0100

    Created an array

---
+++ database.js
@@ -0,0 +1,84 @@
+'use strict'
+
+// My array of coctails
+let database = [
+  {
+    id: 1,
+    name: 'Gin & Tonic',
+    mainIngredient: 'Gin',
+    cl: 4,
+    garniture: 'Lemon'
+  },
+
+  {
+    id: 2,
+    name: 'Whisky Sour',
+    mainIngredient: 'Whisky',
+    cl: 6,
+    garniture: 'None'
+  },
+
+  {
+    id: 3,
+    name: 'Aperol Spritz',
+    mainIngredient: 'Aperol',
+    cl: 4,
+    garniture: 'Orange'
+  },
+
+  {
+    id: 4,
+    name: 'Moscow mule',
+    mainIngredient: 'Vodka',
+    cl: 4,
+    garniture: 'Lime'
+  },
+
+  {
+    id: 5,
+    name: 'Mojito',
+    mainIngredient: 'Rum',
+    cl: 5,
+    garniture: 'Mint'
+  },
+
+  {
+    id: 6,
+    name: 'Caipirinha',
+    mainIngredient: 'Cachaca',
+    cl: 5,
+    garniture: 'Lime'
+  },
+
+  {
+    id: 7,
+    name: 'Amaretto Sour',
+    mainIngredient: 'Amaretto',
+    cl: 6,
+    garniture: 'None'
+  },
+
+  {
+    id: 8,
+    name: 'Gin Fizz',
+    mainIngredient: 'Gin',
+    cl: 4,
+    garniture: 'Lemon'
+  },
+
+  {
+    id: 9,
+    name: 'Mint Julep',
+    mainIngredient: 'Whisky',
+    cl: 6,
+    garniture: 'Mint'
+  },
+
+  {
+    id: 10,
+    name: 'Frozen Strawberry Daiquiri',
+    mainIngredient: 'Rom',
+    cl: 5,
+    garniture: 'Strawberry'
+  }
+]
