0794535f163693334a7f289ee9e482c7134f0458
tbjorklind <97900554+tbjorklind@users.noreply.github.com>
2022-03-01 14:18:54 +0100

    Update main.js
    Created functions that create new objects, add them to the database and render them into HTML elements.

--- main.js
+++ main.js
@@ -1,2 +1,43 @@
-"use strict";
+"use strict"
 
+createNewCoctail();
+renderCoctails(database);
+
+function createNewCoctail (name, liquor, cl, garniture) {
+    let coctail = {
+        name: name,
+        liquor: liquor,
+        cl: cl,
+        garniture: garniture,
+    };
+    return coctail;
+}
+
+function addCoctailToDatabase(database, coctail) {
+  database.push(coctail);
+}
+// Renders a coctail object into a HTML element
+function renderCoctail (coctail) {
+  let div = document.createElement('div')
+  div.classList.add("coctail")
+
+  div.innerHTML = `
+    <div>${coctail.nr}</div>
+    <div>${coctail.name}</div>
+    <div>${coctail.liquor}</div>
+    <div>${coctail.cl}</div>
+    <div>${coctail.garniture}</div>
+    <button>Remove coctail</button>
+    `;
+  return div;
+}
+
+function renderCoctails (coctails) {
+  let coctailsElement = document.getElementById("coctails")
+  coctailsElement.innerHTML = ''
+
+  for (let coctail of coctails) {
+    let coctailElement = renderCoctail(coctail);
+    coctailsElement.appendChild(coctailElement)
+  }
+}
